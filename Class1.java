6fcca0f467a981eb80385b3b6692055c0e66c60a
Deuntae Winston <dequan99.dw@gmail.com.com>
2022-09-03 19:28:09 -0500

    Added f2()

--- Class1.java
+++ Class1.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author Deuntae Winston
  */
 
 public class Class1 {
@@ -8,5 +8,11 @@ public class Class1 {
     private void f1() {
         // v1.0
     }
+    
+     private void f2() {
+        // v2.0
+    }
+    
+    
 
 }
